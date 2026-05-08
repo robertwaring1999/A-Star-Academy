e899b7609a56a391b22261b240786b4d4a7ea97d
robertwaring1990 <robertwaring1990@gmail.com>
2026-05-08 23:26:51 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'a-star-academy-v2';
+const ASSETS = ['./'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./')))
+  );
+});
